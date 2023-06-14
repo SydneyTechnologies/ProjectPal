7c8a1795e3575769fef4000ec27ccb77f80fa1f2
sydneyIdundun <sydney.idundun@gmail.com>
2023-06-14 18:24:52 +0100

    Creating an Instance of the database tables and relationships on start of the server :sparkles: :construction:

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import tables
+from database import engine
 from endpoints import users
+
+tables.BaseTable.metadata.create_all(bind=engine)
 
 DESCRIPTION = "Welcome to Project Pal! Showcase projects, create stunning portfolios. Add details, images, demos.  Effortlessly showcase your work. Join Project Pal today!"
 app = FastAPI(title="ProjectPal Api", description=DESCRIPTION)
